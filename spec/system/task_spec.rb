d263792f15fdfd2c92449e8c28fc84c535fe56ae
masa-oh <makun0084pc@gmail.com>
2021-04-02 18:50:32 +0900

    [Fix]タスク新規作成ページの正常系のテストでletを使うよう修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -26,10 +26,10 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task新規作成' do
+    let(:project) { create(:project) }
+
     context '正常系' do
       it 'Taskが新規作成されること' do
-        # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
         visit project_tasks_path(project)
         click_link 'New Task'
         fill_in 'Title', with: 'test'
